21407a9399b9602e7334c6c701aafbe04aabc211
niki <niki.hp2007@gmail.com>
2013-10-11 12:59:09 +0330

    making forms

---
+++ meet/forms.py
@@ -0,0 +1,12 @@
+from models import *
+from django import forms
+
+class TitleDescriptionForm(forms.ModelForm):
+	class Meta:
+        model = Event
+        fields = ('title', 'description')
+
+class GuestListForm(forms.ModelForm):
+	class Meta:
+		model = Event
+		fields = ('guest_list')
