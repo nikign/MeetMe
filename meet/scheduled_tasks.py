dc1c2dd8c801458eabcdb2a1549972997d2a5e00
ashkan <ashkan.dant3@gmail.com>
2013-10-26 19:20:25 +0330

    Minor changes 3

--- meet/scheduled_tasks.py
+++ meet/scheduled_tasks.py
@@ -1,5 +1,5 @@
-from meet.models import Meeting
-from meet.mail_and_notification
+from meet.models import Meeting, Reservation
+from meet.mail_and_notification import *
 
 #run every 1 hour
 def reserve_room_for_meetings():
@@ -9,6 +9,5 @@ def reserve_room_for_meetings():
 			meeting_reserve = Reservation.reserve_room_for(meeting)
 			inform_admin_reservation(meeting_reserve)
 		except Exception, e:
-			pass
-			#inform the creator
+			inform_no_room_to_owner(meeting)
 
